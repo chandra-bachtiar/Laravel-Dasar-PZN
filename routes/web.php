9e8ffb2e481325af7936790dabbe34a10aa3631e
chandra bachtiar <chandrabahtiar25@gmail.com>
2023-01-27 21:08:16 +0700

    Route Parameter

--- routes/web.php
+++ routes/web.php
@@ -17,24 +17,52 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/chand', function() {
+Route::get('/chand', function () {
     return "Hello Chand!";
 });
 
-Route::redirect('/bachtiar','/chand');
+Route::redirect('/bachtiar', '/chand');
 
-Route::fallback(function(){
+Route::fallback(function () {
     return "404:Not Found!";
 });
 
-Route::view('/hello','hello', ["name"=>'Hello Chand']);
+Route::view('/hello', 'hello', ["name" => 'Hello Chand']);
 
-Route::get('/hello-chand',function(){
-    return view('hello',["name"=>"Hello Chand!"]);
+Route::get('/hello-chand', function () {
+    return view('hello', ["name" => "Hello Chand!"]);
 });
 
-Route::view('/world','hello.world', ["name"=>'World Chand']);
+Route::view('/world', 'hello.world', ["name" => 'World Chand']);
 
-Route::get('/world-chand',function(){
-    return view('hello.world',["name"=>"World Chand!"]);
+Route::get('/world-chand', function () {
+    return view('hello.world', ["name" => "World Chand!"]);
+});
+
+Route::get('/barang/{id}', function ($id) {
+    return "Your ProductId is $id";
+});
+
+Route::get('/barang/{id}/merk/{merk}', function ($id, $merk) {
+    return "Your ProductId $id Merk $merk";
+});
+
+Route::get('/kategori/{id}', function ($id) {
+    return "Kategori : $id";
+})->where('id','[a-z]+');
+
+Route::get('/kategori/{id}/sub/{sub}', function ($id,$sub) {
+    return "Kategori : $id Sub : $sub";
+})->where('id','[a-z]+')->where('sub','[0-9]+');
+
+Route::get('/customer/{id?}',function(string $customer="C01") {
+    return "Customer : $customer";
+});
+
+Route::get('/conflic/chand',function($name){
+    return "Name Chandra bachtiar";
+});
+
+Route::get('/conflic/{name}',function($name){
+    return "Name $name";
 });
--- tests/Feature/RoutingTest.php
+++ tests/Feature/RoutingTest.php
@@ -26,4 +26,42 @@ class RoutingTest extends TestCase
         $this->get('/404')
             ->assertSeeText("404:Not Found!");
     }
+
+    public function testRouteParameter()
+    {
+
+        $this->get('/barang/test')
+            ->assertSeeText("Your ProductId is test");
+
+        $this->get('/barang/test/merk/sanco')
+            ->assertSeeText("Your ProductId test Merk sanco");
+    }
+
+    public function testRouteParameterRgx()
+    {
+        $this->get('/kategori/chand')
+            ->assertSeeText("Kategori : chand");
+
+        $this->get('/kategori/chand/sub/1')
+            ->assertSeeText("Kategori : chand Sub : 1");
+    }
+
+    public function testRouteParameterOptional()
+    {
+        $this->get('/customer/chand')
+            ->assertSeeText("Customer : chand");
+
+        $this->get('/customer')
+            ->assertSeeText("Customer : C01");
+    }
+
+    public function testRouterParameterConflic() {
+        $this->get('/conflic/Bachtiar')
+            ->assertSeeText("Name Bachtiar");
+
+        $this->get('/conflic/chand')
+            ->assertSeeText("Name Chandra bachtiar");
+    }
+
+    
 }
